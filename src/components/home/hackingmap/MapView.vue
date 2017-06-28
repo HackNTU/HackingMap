3f951d03678c05e8e859492af11fc1e580a65f00
choznerol <b02611002@ntu.edu.tw>
2017-06-28 21:43:38 +0800

    完成地圖拖曳、縮放功能(SvgPanZoom)

--- src/components/home/hackingmap/MapView.vue
+++ src/components/home/hackingmap/MapView.vue
@@ -1,50 +1,34 @@
 <template>
-  <div class="mapview">
+  <section class="mapview">
 
-    <!-- 縮放（TODO: 改成scroll放大） -->
-    <div class="block">
-      <el-slider v-model="scale" :max="3" :step="0.1" :format-tooltip="formatTooltip"></el-slider>
-    </div>
+    <!-- SVG -->
+    <svg ref="svg" id="svg-map" class=".svg-pan-zoom_viewport" :viewBox="'0 0 '+map.map_width+' '+map.map_height">
+      <rect x="0" y="0" fill="none" stroke="#EB7B2D" stroke-width="2" :width="map.map_width" :height="map.map_height" />
 
-    <!-- 圖例 -->
-    <div class="legand">
-      <svg height="1.5em" width="5em"><circle cx="1em" cy="0.7em" :r="7" class="statue_proposal"/><text x="2em" y="1em">提案</text></svg>
-      <svg height="1.5em" width="5em"><circle cx="1em" cy="0.7em" :r="7" class="statue_recruit"/><text x="2em" y="1em">徵人</text></svg>
-      <svg height="1.5em" width="5em"><circle cx="1em" cy="0.7em" :r="7" class="statue_hacking"/><text x="2em" y="1em">趕工</text></svg>
-      <svg height="1.5em" width="5em"><circle cx="1em" cy="0.7em" :r="7" class="statue_demo"/><text x="2em" y="1em">展示</text></svg>
-      <svg height="1.5em" width="5em"><circle cx="1em" cy="0.7em" :r="7" class="statue_giveup"/><text x="2em" y="1em">放棄</text></svg>
-    </div>
-
-    <!-- SVG容器 -->
-    <svg id="canvas" xmlns="http://www.w3.org/2000/svg" version="1.1" :width="map.map_width * scale" :height="map.map_height * scale">
       <!-- 地圖 -->
-      <image xlink:href="../../../assets/flora_expo_park.png" x="0" y="0" :width="map.map_width * scale" :height="map.map_height * scale" />
+      <image xlink:href="../../../assets/flora_expo_park.png"
+      :x="0" :y="0" :width="map.map_width" :height="map.map_height"/>
 
+      <!-- 桌子 -->
       <template v-for="table in map.table_coor">
-
-        <!-- 有專案的桌子 -->
         <template v-if="posts[table.no]">
           <el-tooltip placement="top" effect="light">
-            <!-- 專案說明方塊 -->
             <div slot="content">
               <postsummary
-                :title="posts[table.no].name"
-                :subtitle="posts[table.no].author.split('@')[0]"
-                :description="posts[table.no].desc + ' ('+posts[table.no].table+'桌)'"
-                :postKey="posts[table.no]['.key']"
-                :authorId="posts[table.no].uid"
-                :starCount="posts[table.no].starCount"
-                :stars="posts[table.no].stars"
-                :tags="posts[table.no].tags"
-                style="font-family: 'Helvetica Neue',Helvetica,'PingFang SC','Hiragino Sans GB','Microsoft YaHei','微软雅黑',Arial,sans-serif"
-                ></postsummary>
-
+              :title="posts[table.no].name"
+              :subtitle="posts[table.no].author.split('@')[0]"
+              :description="posts[table.no].desc + ' ('+posts[table.no].table+'桌)'"
+              :postKey="posts[table.no]['.key']"
+              :authorId="posts[table.no].uid"
+              :starCount="posts[table.no].starCount"
+              :stars="posts[table.no].stars"
+              :tags="posts[table.no].tags"
+              style="font-family: 'Helvetica Neue',Helvetica,'PingFang SC','Hiragino Sans GB','Microsoft YaHei','微软雅黑',Arial,sans-serif"
+              ></postsummary>
             </div>
-            <circle :cx="table.x * scale" :cy="table.y * scale" r="7" :class="getStatusColor(posts[table.no].status)"/>
+            <circle :cx="table.x" :cy="table.y" r="7" :class="getStatusColor(posts[table.no].status)"/>
           </el-tooltip>
         </template>
-
-        <!-- 沒專案的桌子 -->
         <template v-else>
           <!-- 沒專案的桌子就不顯示圓點 -->
           <!-- <circle :cx="table.x * scale" :cy="table.y * scale" r="7" class="status_avalible"/> -->
@@ -53,40 +37,87 @@
 
     </svg>
 
-  </div>
+    <!-- 圖例 -->
+    <div class="legand">
+      <svg v-for="symble in ['提案', '徵人', '趕工', '展示', '放棄']" height="1.5em" width="5em">
+        <circle :class="getStatusColor(symble)" cx="1em" cy="0.7em" :r="7"/>
+        <text x="2em" y="1em">{{ symble }}</text>
+      </svg>
+    </div>
+
+  </section>
 </template>
 
 <script>
 import appconfig from '../../../appconfig'
 import { VueFireDB } from '@/service/firebase'
+import SvgPanZoom from 'svg-pan-zoom'
 import PostSummary from '@/components/home/hackingmap/postsummary'
 
 export default {
   name: 'mapview',
   data () {
     return {
-      msg: 'MapView.vue',
-      scale: 1.0,
-      map: appconfig.map
+      map: appconfig.map,
+      panZoomMap: null,
+      svgPanZoomOpt: {
+        viewportSelector: '.svg-pan-zoom_viewport',
+        panEnabled: true,
+        controlIconsEnabled: true,
+        dblClickZoomEnabled: true,
+        mouseWheelZoomEnabled: true,
+        preventMouseEventsDefault: true,
+        zoomScaleSensitivity: 0.4,
+        minZoom: 0.9,
+        maxZoom: 5,
+        fit: true
+      },
+      isFetching: true,
+      isMounted: false
     }
   },
-  firebase: {
-    postsData: VueFireDB.ref('posts')
+  firebase () {
+    return {
+      postsData: { source: VueFireDB.ref('posts'),
+        asObject: false,
+        cancelCallback: null,
+        readyCallback: function () {
+          this.posts = this.format(this.postsData)
+          this.isFetching = false
+          console.log('fetched!')
+        }
+      }
+    }
+  },
+  mounted () {
+    console.log('mounted!')
+    this.isMounted = true
   },
   computed: {
-    posts () {
-      let posts = {}
-      this.postsData.forEach((data) => {
-        if (data['table']) {
-          posts[data['table']] = data
-        }
-      })
-      return posts
-    }
+    posts () { return (this.postsData ? this.format(this.postsData) : ({})) },
+    fetchedAndMounted () { return ((!this.isFetching) && this.isMounted) }
+  },
+  updated () {
+    console.log('updated')
+    this.draw()
   },
   methods: {
-    formatTooltip: (val) => String(parseInt(val * 100)) + ' %',
-    getStatusColor: (status) => {
+    draw () {
+      console.log('draw()')
+      let element = this.$refs.svg
+      this.panZoomMap = SvgPanZoom(element, this.svgPanZoomOpt)
+    },
+    format () {
+      if (this.postsData) {
+        // console.log('formatting ', this.postsData)
+        let posts = {}
+        this.postsData
+          .filter(data => data['table'] !== 0)
+          .forEach((data) => { posts[data['table']] = data })
+        return posts
+      }
+    },
+    getStatusColor (status) {
       switch (status) {
         case '提案':
           return 'statue_proposal'
@@ -109,58 +140,40 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="sass" scoped>
 @import "../../../style_config.sass"
-
 .mapview
-  // background-color: $map_bg_color
-  height: 100%
   position: relative
-  overflow-y: scroll
-  overflow-x: scroll
-  right: -17px
-
-#canvas
-  background-color: $map_bg_color
-
-.table
-  fill: none
-  stroke: grey
-
-.map
-  fill: none
-  stroke: black
-
-circle
-  stroke: DarkSlateGrey
-  stroke-width: 2
-
-.statue_proposal
-  fill: $statue_proposal
-
-.statue_recruit
-  fill: $statue_recruit
-
-.statue_hacking
-  fill: $statue_hacking
-
-.statue_demo
-  fill: $statue_demo
-
-.statue_giveup
-  fill: $statue_giveup
-
-.status_avalible
-  fill: white
-
-.legand
-  border: 1px LightGrey solid
-  background-color: rgba(256, 256, 256, 0.8)
-  width: 5em
-  position: absolute
-  top: 30px
-  right: 30px
-  padding: 0.5em
+  z-index: -100
+  top: -52px
+  width: 100%
+  height: 100%
+  #svg-map
+    width: 100%
+    height: 100%
+    background-color: $map_bg_color
+  .legand
+    border: 1px LightGrey solid
+    background-color: rgba(256, 256, 256, 0.8)
+    width: 5em
+    position: absolute
+    top: 30px
+    right: 30px
+    padding: 0.5em
+  .statue_proposal
+    fill: $statue_proposal
+  .statue_recruit
+    fill: $statue_recruit
+  .statue_hacking
+    fill: $statue_hacking
+  .statue_demo
+    fill: $statue_demo
+  .statue_giveup
+    fill: $statue_giveup
+  .status_avalible
+    fill: white
+  circle
+    stroke: DarkSlateGrey
+    stroke-width: 2
 
 </style>
