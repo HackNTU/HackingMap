1acf3c241cff52ebf19dbd11a0d5f5c4d74cafa5
choznerol <b02611002@ntu.edu.tw>
2017-07-11 23:15:47 +0800

    完成搜尋框（可搜尋 tag、標題、簡介）

--- src/components/home/HackingMap.vue
+++ src/components/home/HackingMap.vue
@@ -1,15 +1,45 @@
 <template>
   <div class="hackingmap">
 
-    <el-button-group id="tabs">
-      <router-link to="/map" tag="el-button">地圖</router-link>
-      <router-link to="/projects" tag="el-button">列表</router-link>
-    </el-button-group>
 
-    <router-view v-bind:filteredPosts="posts"></router-view>
+    <div id="toolbar">
+      <el-row>
+
+        <!-- 搜尋框 -->
+        <el-col :span="8">
+          <el-input placeholder="搜尋專案" v-model.lazy.trim="query">
+            <el-select v-model="scope" slot="prepend" placeholder="請選擇" size="large">
+              <el-option label="Tag" value="tags"></el-option>
+              <el-option label="標題" value="name"></el-option>
+              <el-option label="簡介" value="desc"></el-option>
+            </el-select>
+            <!-- <el-button slot="append" icon="search"></el-button> -->
+          </el-input>
+        </el-col>
+
+        <!-- 地圖/列表切換 -->
+        <el-col :span="8">
+          <el-button-group>
+            <router-link to="/map" tag="el-button">地圖</router-link>
+            <router-link to="/projects" tag="el-button">列表</router-link>
+          </el-button-group>
+        </el-col>
+
+        <!-- 排序方法 -->
+        <el-col :span="8">
+          <el-radio-group v-model="sortKey">
+            <el-radio label="heart"><icon name="heart"></icon>數量</el-radio>
+            <el-radio label="star"><icon name="star"></icon>數量</el-radio>
+            <el-radio label="time">建立時間</el-radio>
+          </el-radio-group>
+        </el-col>
+      </el-row>
+    </div>
+
+    <router-view v-bind:filteredPosts="filteredPosts"></router-view>
 
     <template v-if="user !== null">
-      <el-button icon="edit" id="plusBtn" @click="showDialog = true" type="info" :plain="true"></el-button>
+      <el-button icon="edit" id="editBtn" @click="showDialog = true" type="info" :plain="true"></el-button>
       <el-dialog
         :title="myPostTitle"
         :visible.sync="showDialog"
@@ -20,7 +50,7 @@
       </el-dialog>
     </template>
     <template v-else>
-      <el-button icon="edit" id="plusBtn" :disabled="true"></el-button>
+      <el-button icon="edit" id="editBtn" :disabled="true"></el-button>
     </template>
 
   </div>
@@ -28,14 +58,19 @@
 
 <script>
 import { FirebaseApp } from '@/service/firebase.js'
-// import Editor from '@/components/home/hackingmap/Editor.vue'
 import MyPosts from '@/components/home/hackingmap/MyPosts.vue'
+import _ from 'lodash'
+
 export default {
   name: 'hackingmap',
   data () {
     return {
       user: null,
-      showDialog: false
+      showDialog: false,
+      searchScope: [''],
+      query: '',
+      scope: 'tags',
+      sortKey: 'heart'
     }
   },
   firebase: {
@@ -50,6 +85,39 @@ export default {
   computed: {
     myPostTitle () {
       return (this.user.providerData[0].displayName || this.user.email.split('@')[0]) + '的專案'
+    },
+    filteredPosts () {
+      if (!this.query) {
+        return this.posts
+      }
+      switch (this.scope) {
+        case 'tags':
+          return _.pickBy(this.posts, post => {
+            if (post.tags) {
+              let wasFound = false
+              post.tags.forEach((tag) => {
+                if (tag.toLowerCase().includes(this.query.toLowerCase())) {
+                  wasFound = true
+                }
+              })
+              return wasFound
+            }
+          })
+        case 'name':
+          return _.pickBy(this.posts, post => {
+            if (post.name) {
+              return post.name.toLowerCase().includes(this.query.toLowerCase())
+            }
+          })
+        case 'desc':
+          return _.pickBy(this.posts, post => {
+            if (post.desc) {
+              return post.desc.toLowerCase().includes(this.query.toLowerCase())
+            }
+          })
+        default:
+          return this.posts
+      }
     }
   },
   created () {
@@ -62,7 +130,6 @@ export default {
     })
   },
   components: {
-    // editor: Editor
     myposts: MyPosts
   }
 }
@@ -71,20 +138,24 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="sass" scoped>
 @import "../../style_config.sass"
-
 .hackingmap
   background-color: rgba(0, 0, 0, 0)
   position: relative
   height: 100%
   width: 100%
 
-#plusBtn
-  position: absolute
-  bottom: 1rem
-  right: 1rem
+  #editBtn
+    position: absolute
+    bottom: 1rem
+    right: 1rem
 
-#tabs
-  margin: 0.5em
-  position: relative
-  z-index: 100
+  #toolbar
+    margin: 0.5em
+    position: relative
+    z-index: 100
+</style>
+<style>
+.el-select .el-input {
+  width: 80px;
+}
 </style>
