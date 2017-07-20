632a82ab0fe2174ad696d88087a6334d9c1ffd01
choznerol <b02611002@ntu.edu.tw>
2017-07-20 15:19:58 +0800

    修正iOS safari/chrome的導覽iframe overflow問題

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,13 +1,16 @@
 <template lang="html">
   <section class="intro">
-    <div class="wrap loading-bg">
-      <iframe
-        :src="url"
-        @load="isLoaded = true"
-        v-show="isLoaded"
-        width="100%"
-        height="100%">
-      </iframe>
+    <div class="wrap">
+      <div class="loading-bg scroll-wrapper">
+        <iframe
+          id="intro-iframe"
+          :src="url"
+          @load="isLoaded = true"
+          v-show="isLoaded"
+          width="100%"
+          height="100%">
+        </iframe>
+      </div>
     </div>
   </section>
 </template>
@@ -29,4 +32,21 @@ export default {
     height: 50vh
   .loading-bg
     background:url(../assets/loading.gif) center center no-repeat
+  .warp, #intro-iframe
+
+  .scroll-wrapper
+    border: 1px dashed Grey
+    position: fixed
+    right: 0
+    bottom: 4.5em
+    left: 0
+    top: 4.5em
+    -webkit-overflow-scrolling: touch
+    overflow-y: scroll
+
+  .scroll-wrapper iframe
+    border: 1px solid LightGrey
+    height: 100%
+    width: 100%
+
 </style>
