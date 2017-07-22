1f4b8f485defc09387428973a5548e1f4a9e9ce6
choznerol <b02611002@ntu.edu.tw>
2017-07-22 18:13:19 +0800

    UI微調

--- src/components/home/hackingmap/PostSummary.vue
+++ src/components/home/hackingmap/PostSummary.vue
@@ -24,9 +24,11 @@
       <span class="raCount">{{ heartCount }}</span>
     </span>
 
-    <h3 id="title">{{ host }},{{ title }}</h3>
+    <h3 id="title">{{ title }}</h3>
     <!-- <h5>{{ subtitle }}</h5> -->
-    <p class="description">{{ description }}</p>
+    <p class="description">
+      <strong>{{ host }}</strong>
+      {{ description }}</p>
 
     <div class="tags" style="width:100%;">
       <el-button
